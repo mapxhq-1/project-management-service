e0fe09d59ee3a7d7364a2334e02e02ddd908d67f
Pranee345 <praneethvineeth9@gmail.com>
2025-09-08 01:22:14 +0530

    Final Commit of all apis which are not modified with latest updated for modification

--- src/main/java/com/example/ProjectManagement/Exception/InvalidProjectIdException.java
+++ src/main/java/com/example/ProjectManagement/Exception/InvalidProjectIdException.java
@@ -2,6 +2,6 @@ package com.example.ProjectManagement.Exception;
 
 public class InvalidProjectIdException extends RuntimeException{
     public InvalidProjectIdException(){
-        super("Project with given project Id is not found");
+        super("Projec with given project Id is not found");
     }
 }

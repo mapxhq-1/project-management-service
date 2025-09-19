3055026993336301835e34371570a83f628e7851
Pranee345 <praneethvineeth9@gmail.com>
2025-09-20 01:13:20 +0530

    Added all the controller and serivce and reposiory of map Shapes

---
+++ src/main/java/com/example/ProjectManagement/repository/MapShapesRepository.java
@@ -0,0 +1,28 @@
+package com.example.ProjectManagement.repository;
+
+
+import com.example.ProjectManagement.model.HistoricalYear;
+import com.example.ProjectManagement.model.MapShapes;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface MapShapesRepository extends MongoRepository<MapShapes,String> {
+
+
+    // Fetch from DB
+
+    List<MapShapes> findByProjectIdAndYearInTimeline(
+             String projectId,
+             HistoricalYear yearInTimeline
+    );
+
+
+
+
+
+
+    }

892516adf55708c800f8b68800f273ea5c7e0a84
sapta <www.saptarsi@gmail.com>
2025-10-13 00:31:34 +0200

    added mongoconfig and webconfig

---
+++ src/main/java/com/example/ProjectManagement/config/MongoConfig.java
@@ -0,0 +1,11 @@
+package com.example.ProjectManagement.config;
+
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
---
+++ src/main/java/com/example/ProjectManagement/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.ProjectManagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("https://app.mapx.in", "https://mapdesk.mapx.in", "https://mapx-geo-json-service.netlify.app") // Allow your frontend
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}

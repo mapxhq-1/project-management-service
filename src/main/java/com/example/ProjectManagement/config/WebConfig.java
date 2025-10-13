1cf70c56615ff7dc3cf3352f3a1f1c8fb102eed0
sapta <www.saptarsi@gmail.com>
2025-10-14 00:30:38 +0200

    some more cors

--- src/main/java/com/example/ProjectManagement/config/WebConfig.java
+++ src/main/java/com/example/ProjectManagement/config/WebConfig.java
@@ -1,19 +1,28 @@
 package com.example.ProjectManagement.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+@EnableWebMvc
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply to all endpoints
-                .allowedOrigins("https://app.mapx.in", "https://api.mapx.in", "https://mapx-web.netlify.app", "https://mapdesk.mapx.in", "https://mapx-geo-json-service.netlify.app", "http://localhost") // Your frontend URL
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true) // If you use cookies or HTTP authentication
-                .maxAge(3600); // Cache preflight response for 1 hour
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Allow all paths
+                        .allowedOrigins("https://app.mapx.in", "https://api.mapx.in", "https://mapx-web.netlify.app", "https://mapdesk.mapx.in", "https://mapx-geo-json-service.netlify.app", "http://localhost") // Allow your frontend
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }

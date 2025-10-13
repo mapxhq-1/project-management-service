89d132967000ca25d6e559dd3bf68948f278daf4
sapta <www.saptarsi@gmail.com>
2025-10-14 00:55:25 +0200

    removed credentials

--- src/main/java/com/example/ProjectManagement/config/WebConfig.java
+++ src/main/java/com/example/ProjectManagement/config/WebConfig.java
@@ -20,8 +20,7 @@ public class WebConfig {
                 registry.addMapping("/**") // Allow all paths
                         .allowedOriginPatterns("https://app.mapx.in", "https://api.mapx.in", "https://mapx-web.netlify.app", "https://mapdesk.mapx.in", "https://mapx-geo-json-service.netlify.app", "http://localhost") // Allow your frontend
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }

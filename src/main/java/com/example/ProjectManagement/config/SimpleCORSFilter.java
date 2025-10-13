8cee8c879fd1ac5c504ce6538d4315bd6761e670
sapta <www.saptarsi@gmail.com>
2025-10-14 00:45:40 +0200

    added precedence

--- src/main/java/com/example/ProjectManagement/config/SimpleCORSFilter.java
+++ src/main/java/com/example/ProjectManagement/config/SimpleCORSFilter.java
@@ -4,10 +4,12 @@ package com.example.ProjectManagement.config;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
+@Order(0)  // highest precedence
 public class SimpleCORSFilter implements Filter {
 
     @Override

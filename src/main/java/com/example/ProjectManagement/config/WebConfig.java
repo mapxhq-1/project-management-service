d12f018466b1fbaa56d973967ed5f91cdfbf8c93
sapta <www.saptarsi@gmail.com>
2025-10-13 23:54:17 +0200

    trial 2 changing application.properties

--- src/main/java/com/example/ProjectManagement/config/WebConfig.java
+++ src/main/java/com/example/ProjectManagement/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Allow all paths
-                        .allowedOrigins("https://app.mapx.in", "https://*.netlify.app", "https://mapx-web.netlify.app", "https://mapdesk.mapx.in", "https://mapx-geo-json-service.netlify.app", "http://localhost") // Allow your frontend
+                        .allowedOrigins("https://app.mapx.in", "https://api.mapx.in", "https://*.netlify.app", "https://mapx-web.netlify.app", "https://mapdesk.mapx.in", "https://mapx-geo-json-service.netlify.app", "http://localhost") // Allow your frontend
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);

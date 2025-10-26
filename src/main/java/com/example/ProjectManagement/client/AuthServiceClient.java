d7c346ba119ad6a9ffdf2ce5205e65cc56ae6e97
Pranee345 <praneethvineeth9@gmail.com>
2025-10-26 18:46:10 +0530

    Hello

--- src/main/java/com/example/ProjectManagement/client/AuthServiceClient.java
+++ src/main/java/com/example/ProjectManagement/client/AuthServiceClient.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+
 @Component
 public class AuthServiceClient {
 
